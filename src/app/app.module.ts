abcc2f6dec8ad8fe0ce92c80c5fade3e1ee96725
Miroslav Stefanović <mstef@uns.ac.rs>
2020-05-03 17:06:55 +0200

    Vežbe 07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VoziloComponent } from './vozilo/vozilo.component';
+import { AutomobilComponent } from './vozilo/automobil/automobil.component';
+import { ArtiklComponent } from './artikl/artikl.component';
+import { DobavljacComponent } from './dobavljac/dobavljac.component';
+import { PorudzbinaComponent } from './porudzbina/porudzbina.component';
+import { StavkaPorudzbineComponent } from './stavka-porudzbine/stavka-porudzbine.component';
+import { HomeComponent } from './core/home/home.component';
+import { AuthorComponent } from './core/author/author.component';
+import { AboutComponent } from './core/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoziloComponent,
+    AutomobilComponent,
+    ArtiklComponent,
+    DobavljacComponent,
+    PorudzbinaComponent,
+    StavkaPorudzbineComponent,
+    HomeComponent,
+    AuthorComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
